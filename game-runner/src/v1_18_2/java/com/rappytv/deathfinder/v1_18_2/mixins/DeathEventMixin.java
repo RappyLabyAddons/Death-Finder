7d1b6fd39d422fd8d9a858235bad2f5b8cfd6cfd
RappyTV <rappytv@rappytv.com>
2023-04-28 19:29:38 +0200

    Added 1.18.2 mixin

---
+++ game-runner/src/v1_18_2/java/com/rappytv/deathfinder/v1_18_2/mixins/DeathEventMixin.java
@@ -0,0 +1,30 @@
+package com.rappytv.deathfinder.v1_18_2.mixins;
+
+import com.rappytv.deathfinder.DeathFinderAddon;
+import com.rappytv.deathfinder.events.DeathEvent;
+import com.rappytv.deathfinder.util.Location;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.level.gameevent.GameEvent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Entity.class)
+public abstract class DeathEventMixin {
+
+    @Inject(method = "gameEvent(Lnet/minecraft/world/level/gameevent/GameEvent;Lnet/minecraft/world/entity/Entity;)V", at = @At("HEAD"))
+    public void onDeath(GameEvent event, Entity entity, CallbackInfo ci) {
+        if(event == GameEvent.ENTITY_KILLED && entity.getType() == EntityType.PLAYER) {
+            Location deathLocation = new Location(entity.getX(), entity.getY(), entity.getZ());
+
+            if(DeathFinderAddon.get().configuration().saveRotation().get()) {
+                deathLocation.setYaw(entity.getXRot());
+                deathLocation.setPitch(entity.getYRot());
+            }
+
+            new DeathEvent(deathLocation);
+        }
+    }
+}
