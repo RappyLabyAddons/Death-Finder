1f2685978fd7a7bfbefec8362c7d98e18e5434bb
RappyTV <rappytv@rappytv.com>
2023-04-28 16:59:07 +0200

    Added 1.19.3 mixin

---
+++ game-runner/src/v1_19_3/java/com/rappytv/back/v1_19_3/mixins/DeathEventMixin.java
@@ -0,0 +1,30 @@
+package com.rappytv.back.v1_19_3.mixins;
+
+import com.rappytv.back.BackAddon;
+import com.rappytv.back.events.DeathEvent;
+import com.rappytv.back.util.Location;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.level.gameevent.GameEvent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Entity.class)
+public abstract class DeathEventMixin {
+
+    @Inject(method = "gameEvent(Lnet/minecraft/world/level/gameevent/GameEvent;Lnet/minecraft/world/entity/Entity;)V", at = @At("HEAD"))
+    public void onDeath(GameEvent event, Entity entity, CallbackInfo ci) {
+        if(event == GameEvent.ENTITY_DIE && entity.getType() == EntityType.PLAYER) {
+            Location deathLocation = new Location(entity.getX(), entity.getY(), entity.getZ());
+
+            if(BackAddon.get().configuration().saveRotation().get()) {
+                deathLocation.setYaw(entity.getXRot());
+                deathLocation.setPitch(entity.getYRot());
+            }
+
+            new DeathEvent(deathLocation);
+        }
+    }
+}
