aacf841e7c5dff0fb2632be7277c1d497c3d5963
RappyTV <rappytv@rappytv.com>
2023-09-23 14:27:12 +0200

    Remove unused function

--- core/src/main/java/com/rappytv/deathfinder/util/Util.java
+++ core/src/main/java/com/rappytv/deathfinder/util/Util.java
@@ -5,15 +5,8 @@ import static com.rappytv.deathfinder.DeathFinderAddon.prefix;
 import com.rappytv.deathfinder.DeathFinderAddon;
 import net.labymod.api.client.component.Component;
 import net.labymod.api.client.component.format.NamedTextColor;
-import net.labymod.api.util.I18n;
 
 public class Util {
-    public static String getTranslation(String key, Object... args) {
-        if(!I18n.has(key))
-            return key;
-        return I18n.getTranslation(key, args);
-    }
-
     public static void msg(Component... lines) {
         Component component = Component
             .text("»\n", NamedTextColor.DARK_GRAY);
