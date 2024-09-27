452cb3f233105f1e2fe7a44c6ded58f3b37b2559
RappyTV <rappytv@rappytv.com>
2024-09-27 19:34:06 +0200

    Implement v1.21.1 support

---
+++ game-runner/src/v1_21_1/java/com/rappytv/deathfinder/v1_21_1/mixins/DeathScreenMixin.java
@@ -0,0 +1,41 @@
+package com.rappytv.deathfinder.v1_21_1.mixins;
+
+import com.rappytv.deathfinder.DeathFinderAddon;
+import com.rappytv.deathfinder.events.DeathEvent;
+import com.rappytv.deathfinder.util.DeathLocation;
+import net.labymod.api.Laby;
+import net.labymod.api.client.entity.Entity;
+import net.labymod.api.client.entity.player.ClientPlayer;
+import net.minecraft.client.gui.screens.DeathScreen;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(DeathScreen.class)
+public class DeathScreenMixin extends Screen {
+
+    protected DeathScreenMixin(Component title) {
+        super(title);
+    }
+
+    @Inject(method = "init", at = @At("TAIL"))
+    public void onDeathScreen(CallbackInfo ci) {
+        ClientPlayer player = Laby.labyAPI().minecraft().getClientPlayer();
+        if(player == null) return;
+
+        DeathLocation deathLocation = new DeathLocation(
+            player.getPosX(),
+            player.getPosY(),
+            player.getPosZ(),
+            ((Entity) player).getRotationYaw(),
+            ((Entity) player).getRotationPitch()
+        );
+
+        if(deathLocation.equals(DeathFinderAddon.getDeathLocation())) return;
+
+        Laby.fireEvent(new DeathEvent(deathLocation));
+    }
+}
