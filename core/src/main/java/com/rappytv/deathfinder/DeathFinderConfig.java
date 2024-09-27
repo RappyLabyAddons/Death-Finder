ef7551a1736def26baffc758b5de43000ea47ec4
RappyTV <rappytv@rappytv.com>
2024-09-27 18:42:25 +0200

    Remove unnecessary options, add hotkey to enabled boolean

--- core/src/main/java/com/rappytv/deathfinder/DeathFinderConfig.java
+++ core/src/main/java/com/rappytv/deathfinder/DeathFinderConfig.java
@@ -9,30 +9,11 @@ import net.labymod.api.configuration.settings.annotation.SettingSection;
 @ConfigName("settings")
 public class DeathFinderConfig extends AddonConfig {
 
-    @SettingSection("general")
-    @SwitchSetting
+    @SwitchSetting(hotkey = true)
     private final ConfigProperty<Boolean> enabled = new ConfigProperty<>(true);
-    @SwitchSetting
-    private final ConfigProperty<Boolean> saveRotation = new ConfigProperty<>(false);
-
-    @SettingSection("commands")
-    @SwitchSetting
-    private final ConfigProperty<Boolean> backCommand = new ConfigProperty<>(true);
-    @SwitchSetting
-    private final ConfigProperty<Boolean> coordsCommand = new ConfigProperty<>(true);
 
     @Override
     public ConfigProperty<Boolean> enabled() {
         return enabled;
-    }
-    public ConfigProperty<Boolean> saveRotation() {
-        return saveRotation;
-    }
-
-    public ConfigProperty<Boolean> backCommand() {
-        return backCommand;
-    }
-    public ConfigProperty<Boolean> coordsCommand() {
-        return coordsCommand;
     }
 }
