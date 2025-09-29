996c989cf814ce95dd637b664106cf7f17dd64b2
RappyTV <rappytv@rappytv.com>
2025-09-29 02:36:08 +0200

    chore: Optimize imports

--- core/src/main/java/com/rappytv/deathfinder/DeathFinderConfig.java
+++ core/src/main/java/com/rappytv/deathfinder/DeathFinderConfig.java
@@ -4,7 +4,6 @@ import net.labymod.api.addon.AddonConfig;
 import net.labymod.api.client.gui.screen.widget.widgets.input.SwitchWidget.SwitchSetting;
 import net.labymod.api.configuration.loader.annotation.ConfigName;
 import net.labymod.api.configuration.loader.property.ConfigProperty;
-import net.labymod.api.configuration.settings.annotation.SettingSection;
 
 @ConfigName("settings")
 public class DeathFinderConfig extends AddonConfig {
